676621aebe9301f540c974609d147dfc85167af2
loveZero <1120877019@qq.com>
2021-08-01 00:02:19 +0800

    vuex尝试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
-  <!-- vue2.0下必须要有一个根元素，vue3可以使代码片段 Fragment -->
-  <div>
-    App
-  </div>
-  <p></p>
+  <!-- 模块A -->
+  <!-- 不开启命名空间，只有state可以加模块名，getters，mutations，actions都是全局的 -->
+  <div>{{ $store.state.moduleA.userName }}</div>
+  <!-- <div>{{ $store.getters.moduleA.userName }}</div> -->
+  <div>{{ $store.getters.changeName }}</div>
+  <!-- 模块B -->
+  <div>{{ $store.state.moduleB.userName }}</div>
+  <div>{{ $store.getters['moduleB/changeName'] }}</div>
+  <button @click="mutationsFn">mutationsFn</button>
+  <button @click="actionsFn">actionsFn</button>
 </template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
+    // 使用vuex仓库
+    const store = useStore()
+    // 调用B模块的mutations
+    const mutationsFn = () => {
+      store.commit('moduleB/update')
+    }
+    // 调用B模块的actions
+    const actionsFn = () => {
+      store.dispatch('moduleB/update')
+    }
+    return { mutationsFn, actionsFn }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,49 @@
 import { createStore } from 'vuex'
+const moduleA = {
+  // 子模块state建议写成函数
+  state: () => {
+    return {
+      userName: '模块A'
+    }
+  },
+  getters: {
+    changeName (state) {
+      return state.userName + 'AAAAA'
+    }
+  }
+}
 
+const moduleB = {
+  namespaced: true,
+  // 子模块state建议写成函数
+  state: () => {
+    return {
+      userName: '模块B'
+    }
+  },
+  getters: {
+    changeName (state) {
+      return state.userName + 'BBBBB'
+    }
+  },
+  mutations: {
+    // 修改名字的mutation
+    update (state) {
+      state.userName = 'BBBB' + state.userName
+    }
+  },
+  actions: {
+    update ({ commit }) {
+      // 假设请求
+      setTimeout(() => {
+        commit('update')
+      }, 2000)
+    }
+  }
+}
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    moduleA,
+    moduleB
+  }
 })
