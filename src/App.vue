cebe989fffd62e990e30157e8e2a69df8a822daa
loveZero <1120877019@qq.com>
2021-07-31 21:52:26 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <!-- vue2.0下必须要有一个根元素，vue3可以使代码片段 Fragment -->
+  <div>
+    App
   </div>
-  <router-view/>
+  <p></p>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// 路由规则
+const routes = []
 
 const router = createRouter({
+  // 使用功能hash的路由模式
   history: createWebHashHistory(),
   routes
 })
