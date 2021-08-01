1e955a22f17f31cbd6d99c2feff53b76e924a95e
loveZero <1120877019@qq.com>
2021-08-01 18:38:52 +0800

    footer完成

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -8,16 +8,21 @@
     <Routerview />
   </div>
   <!-- 底部 -->
-  <footer>底部</footer>
+  <Footer />
 </template>
 
 <script>
 import NavBar from '@/components/NavBar'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
   name: 'Layout',
-  components: { NavBar, Header }
+  components: { NavBar, Header, Footer }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.main {
+  height: 600px;
+}
+</style>
