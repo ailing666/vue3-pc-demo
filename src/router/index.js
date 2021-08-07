c2e1e360ce5bd31f0d22b8e9b96cc006aad585be
loveZero <1120877019@qq.com>
2021-08-07 23:04:33 +0800

    每次切换路由时。滚到路由的顶部

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,11 @@ const routes = [
 const router = createRouter({
   // 使用hash的路由模式
   history: createWebHashHistory(),
-  routes
+  routes,
+  // 每次切换路由时。滚到路由的顶部
+  scrollBehavior () {
+    return { left: 0, top: 0 }
+  }
 })
 
 export default router
