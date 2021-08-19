5301aedd032cd239eed9d1eb798e0b9b017ca99a
loveZero <1120877019@qq.com>
2021-08-20 00:06:22 +0800

    pathMap函数得到子集

--- src/views/goods/components/GoodsSku.vue
+++ src/views/goods/components/GoodsSku.vue
@@ -23,6 +23,34 @@
   </div>
 </template>
 <script>
+import powerSet from '@/vender/power-set'
+const spliter = '★'
+// 得到一个路径字典对象
+const getPathMap = skus => {
+  const pathMap = {}
+  skus.forEach(sku => {
+    // 从所有的sku中筛选出有效的sku，sku.inventory > 0表示有存货
+    if (sku.inventory > 0) {
+      // 可选值数组
+      const valueArr = sku.specs.map(val => val.valueName)
+      // 使用power-set算法得到子集
+      const valueArrPowerSet = powerSet(valueArr)
+      // 遍历子集，往pathMap插入数据
+      valueArrPowerSet.forEach(arr => {
+        // 根据arr得到字符串的key，约定key是：['蓝色','美国'] ===> '蓝色★美国'
+        const key = arr.join(spliter)
+        // 根据子集往路径字典对象中存储 key-value
+        // 给pathMap设置数据，如果已经有，就push，否者就创建
+        if (pathMap[key]) {
+          pathMap[key].push(sku.id)
+        } else {
+          pathMap[key] = [sku.id]
+        }
+      })
+    }
+  })
+  return pathMap
+}
 export default {
   name: 'GoodsSku',
   props: {
@@ -32,6 +60,8 @@ export default {
     }
   },
   setup (props) {
+    const pathMap = getPathMap(props.goods.skus)
+    console.log('pathMap: ', pathMap)
     const clickSpecs = (item, val) => {
       // 选中与取消选中逻辑
       if (val.selected) {
