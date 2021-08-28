c4e11c3e740a58adffc93add21e10b4d59388fa3
loveZero <1120877019@qq.com>
2021-08-28 23:39:27 +0800

    猜你喜欢

--- src/views/goods/components/GoodsRelevant.vue
+++ src/views/goods/components/GoodsRelevant.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="goods-relevant"></div>
+  <div class="goods-relevant">
+    <div class="header">
+      <i class="icon" />
+      <span class="title">{{ goodsId ? '同类商品推荐' : '猜你喜欢' }}</span>
+    </div>
+    <Carousel :sliders="sliders" style="height:380px" auto-play />
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { findRelevantGoods } from '@/api/goods'
 export default {
-  name: 'GoodsRelevant'
+  // 同类推荐，猜你喜欢
+  name: 'GoodsRelevant',
+  props: {
+    goodsId: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props) {
+    const sliders = ref([])
+    findRelevantGoods({ id: props.goodsId }).then(data => {
+      const pageSize = 4
+      const pageCount = Math.ceil(data.result.length / pageSize)
+      for (let i = 0; i < pageCount; i++) {
+        sliders.value.push(data.result.slice(pageSize * i, pageSize * (i + 1)))
+      }
+    })
+    return { sliders }
+  }
 }
 </script>
 
@@ -13,5 +39,55 @@ export default {
   background: #fff;
   min-height: 460px;
   margin-top: 20px;
+  .header {
+    height: 80px;
+    line-height: 80px;
+    padding: 0 20px;
+    .title {
+      font-size: 20px;
+      padding-left: 10px;
+    }
+    .icon {
+      width: 16px;
+      height: 16px;
+      display: inline-block;
+      border-top: 4px solid @mainColor;
+      border-right: 4px solid @mainColor;
+      box-sizing: border-box;
+      position: relative;
+      transform: rotate(45deg);
+      &::before {
+        content: '';
+        width: 10px;
+        height: 10px;
+        position: absolute;
+        left: 0;
+        top: 2px;
+        background: lighten(@mainColor, 40%);
+      }
+    }
+  }
+}
+:deep(.carousel) {
+  height: 380px;
+  .carousel {
+    &-indicator {
+      bottom: 30px;
+      span {
+        &.active {
+          background: @mainColor;
+        }
+      }
+    }
+    &-btn {
+      top: 110px;
+      opacity: 1;
+      background: rgba(0, 0, 0, 0);
+      color: #ddd;
+      i {
+        font-size: 30px;
+      }
+    }
+  }
 }
 </style>
